1ab8ffa230d8c28e1659f228731576b0672f7ee0
bezbrain <belloquadriolawale@gmail.com>
2024-02-18 21:11:36 +0100

    Handle pricing containers better

--- src/components/routes/home/pricing/priceCon/priceCard.tsx
+++ src/components/routes/home/pricing/priceCon/priceCard.tsx
@@ -11,7 +11,7 @@ interface PriceCardProps {
   planFive: string;
 }
 
-const listStyle = "flex items-center gap-1 mt-4";
+const listStyle = "flex items-center gap-1 mt-1 surfaceDuo:mt-4";
 const textSize = "text-sm";
 
 const PriceCard = ({
@@ -25,10 +25,12 @@ const PriceCard = ({
   planFive,
 }: PriceCardProps) => {
   return (
-    <div className="w-full pt-4 pb-8 rounded-md cursor-pointer border-2 border-[#bdd3f4cc] px-[2vw] lg:hover:scale-[1.20] iPad:hover:scale-[1.10] md:hover:scale-[1.10] iPad:px-12 md:hover:bg-[#1E3448] md:hover:border-0 md:hover:text-white transition-transform surfaceDuo:w-fit">
+    <div className="w-full p-2 rounded-md cursor-pointer border-2 border-[#bdd3f4cc] px-[2vw] lg:hover:scale-[1.20] iPad:hover:scale-[1.10] md:hover:scale-[1.10] iPad:px-10 md:hover:bg-[#1E3448] md:hover:border-0 md:hover:text-white transition-transform surfaceDuo:w-fit surfaceDuo:pt-4 surfaceDuo:pb-8">
       <p>{type}</p>
-      <h2 className="text-3xl font-semibold mt-8">{price}</h2>
-      <h4 className="my-2">{title}</h4>
+      <h2 className="text-xl font-semibold mt-2 surfaceDuo:mt-8 surfaceDuo:text-3xl">
+        {price}
+      </h2>
+      <h4 className="my-1 surfaceDuo:my-2">{title}</h4>
 
       <ul>
         <li className={listStyle}>
