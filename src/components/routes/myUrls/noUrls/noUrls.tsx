6b075ea8749bcd86ae341d184513903cea66a310
bezbrain <belloquadriolawale@gmail.com>
2024-02-22 17:14:33 +0100

    Overflow the table on the y-axis

--- src/components/routes/myUrls/noUrls/noUrls.tsx
+++ src/components/routes/myUrls/noUrls/noUrls.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const NoUrls = () => {
   return (
-    <NoUrlWrapper className="w-fit absolute right-[50%] top-[10vh] translate-x-[50%] text-center">
+    <NoUrlWrapper className="w-fit absolute right-[50%] top-[40vh] translate-x-[50%] text-center">
       <p className="text-3xl text-red-900 font-semibold">No URLs</p>
       <img
         src="https://th.bing.com/th/id/OIP.wcqMAgXJcsFC8Ba5ndfY7QHaEY?w=297&h=180&c=7&r=0&o=5&pid=1.7"
@@ -15,6 +15,17 @@ const NoUrls = () => {
 export default NoUrls;
 
 const NoUrlWrapper = styled.div`
+  @media screen and (max-height: 640px) {
+    /* border: 2px solid red; */
+    top: 250px !important;
+    > p {
+      font-size: 25px !important;
+    }
+    > img {
+      width: 200px;
+    }
+  }
+
   > img {
     /* border: 2px solid red; */
     margin-top: 10px;
