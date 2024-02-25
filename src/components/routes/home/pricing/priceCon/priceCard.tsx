4793042e61ed1ff4fe5dbe0e87fc97ea5d34bbbe
bezbrain <belloquadriolawale@gmail.com>
2024-02-25 21:25:04 +0100

    Add box shadow to each price plan card

--- src/components/routes/home/pricing/priceCon/priceCard.tsx
+++ src/components/routes/home/pricing/priceCon/priceCard.tsx
@@ -9,7 +9,7 @@ interface PriceCardProps {
   planThree: string;
   planFour: string;
   planFive: string;
-  setIsComingSoon: React.Dispatch<React.SetStateAction<boolean>>
+  setIsComingSoon: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const listStyle = "flex items-center gap-1 mt-1 surfaceDuo:mt-4";
@@ -24,10 +24,13 @@ const PriceCard = ({
   planThree,
   planFour,
   planFive,
-  setIsComingSoon
+  setIsComingSoon,
 }: PriceCardProps) => {
   return (
-    <div className="w-full p-2 rounded-md cursor-pointer border-2 border-[#bdd3f4cc] px-[2vw] lg:hover:scale-[1.20] iPad:hover:scale-[1.10] md:hover:scale-[1.10] iPad:px-10 md:hover:bg-[#1E3448] md:hover:border-0 md:hover:text-white transition-transform surfaceDuo:w-fit surfaceDuo:pt-4 surfaceDuo:pb-8" onClick={()=>setIsComingSoon(true)}>
+    <div
+      className="w-full p-2 rounded-md cursor-pointer border-2 border-[#bdd3f4cc] px-[2vw] shadow-md shadow-slate-700 lg:hover:scale-[1.20] iPad:hover:scale-[1.10] md:hover:scale-[1.10] iPad:px-10 md:hover:bg-[#1E3448] md:hover:border-0 md:hover:text-white transition-transform surfaceDuo:w-fit surfaceDuo:pt-4 surfaceDuo:pb-8"
+      onClick={() => setIsComingSoon(true)}
+    >
       <p>{type}</p>
       <h2 className="text-xl font-semibold mt-2 surfaceDuo:mt-8 surfaceDuo:text-3xl">
         {price}
