55b49ddc72f5d5336d6f5a69d0449202b8e94ac1
bezbrain <belloquadriolawale@gmail.com>
2024-03-02 17:14:45 +0100

    Add radial gradient to the learn more page

--- src/pages/learnMore.page.tsx
+++ src/pages/learnMore.page.tsx
@@ -1,11 +1,22 @@
+import styled from "styled-components";
 import { LearnMore } from "../components/routes/learnMore";
 
 const LearnMorePage = () => {
   return (
-    <main className="pt-[60px] min-h-[100vh]">
+    <LearnMoreWrapper className="pt-[60px] min-h-[100vh]">
       <LearnMore />
-    </main>
+    </LearnMoreWrapper>
   );
 };
 
 export default LearnMorePage;
+
+const LearnMoreWrapper = styled.main`
+  background: radial-gradient(
+    circle at 100%,
+    #586d80,
+    #eee 75%,
+    #536a7f 50%,
+    #dae3eb 75%
+  );
+`;
