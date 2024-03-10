939929668502b38d186b675776090dba6e31b91d
bezbrain <belloquadriolawale@gmail.com>
2024-03-10 15:26:40 +0100

    Integrate the api for engagements metrics and urls

---
+++ src/management/actions/analytics.action.ts
@@ -0,0 +1,30 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { getAnalytics, getCusAnalytics } from "../services/analytics";
+
+export const allAnalytics = createAsyncThunk(
+  "action/allAnalytics",
+  async (_, thunkAPI) => {
+    try {
+      const { data } = await getAnalytics();
+      console.log(data);
+      return data;
+    } catch (error) {
+      console.log(error);
+      thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+export const allCusAnalytics = createAsyncThunk(
+  "action/allCusAnalytics",
+  async (_, thunkAPI) => {
+    try {
+      const { data } = await getCusAnalytics();
+      console.log(data);
+      return data;
+    } catch (error) {
+      console.log(error);
+      thunkAPI.rejectWithValue(error);
+    }
+  }
+);
---
+++ src/management/features/analytics/analyticsSlice.tsx
@@ -0,0 +1,66 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { allAnalytics, allCusAnalytics } from "../../actions/analytics.action";
+
+interface Analytics {
+  analyze: {
+    totalUsers: string;
+    screenPageViews: string;
+    userEngagement: string;
+    shortUrlVisited: string;
+    CustomUrlVisited: string;
+  };
+  isLoading: boolean;
+}
+
+const initialState: Analytics = {
+  analyze: {
+    totalUsers: "",
+    screenPageViews: "",
+    userEngagement: "",
+    shortUrlVisited: "",
+    CustomUrlVisited: "",
+  },
+  isLoading: false,
+};
+
+const analyticsSlice = createSlice({
+  name: "analytics",
+  initialState,
+  reducers: {
+    //
+  },
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(allAnalytics.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(allAnalytics.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        // console.log(payload.urlAnalyticsData);
+        state.analyze = payload.urlAnalyticsData;
+      })
+      .addCase(allAnalytics.rejected, (state) => {
+        state.isLoading = false;
+      })
+      // Custom get analytics extrareducer
+      .addCase(allCusAnalytics.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(allCusAnalytics.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        // console.log(payload.urlAnalyticsData);
+        state.analyze = {
+          ...state.analyze,
+          ...payload.urlAnalyticsData,
+        };
+      })
+      .addCase(allCusAnalytics.rejected, (state) => {
+        state.isLoading = false;
+      });
+  },
+});
+
+export default analyticsSlice.reducer;
+
+// export const {} = analyticsSlice.actions;
