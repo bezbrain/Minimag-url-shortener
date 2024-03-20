5f1fefe7cc8f67d2b64092661646a8a0fe960f81
bezbrain <belloquadriolawale@gmail.com>
2024-03-20 09:56:32 +0100

    Write test to make sure hero page renders

--- src/__test__/homepage.test.tsx
+++ src/__test__/homepage.test.tsx
@@ -1,14 +1,17 @@
 import { render, screen } from "@testing-library/react";
-// import "@testing-library/jest-dom";
-// import { describe } from "vitest";
 import { Hero } from "../components/routes/home";
+import { MemoryRouter } from "react-router-dom"; // Use this to handle the error thrown because react-router-dom
 
 describe("Homepage", () => {
   // Test if hero section renders correctly
   it("renders Hero section", () => {
     // const setIsComingSoonMock = jest.fn(); // Mock function for setIsComingSoon prop
 
-    render(<Hero />);
+    render(
+      <MemoryRouter>
+        <Hero />
+      </MemoryRouter>
+    );
 
     const heroSection = screen.getByTestId("hero-section");
     expect(heroSection).toBeInTheDocument();
